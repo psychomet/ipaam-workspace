c2767774b9f130b8297c4c3531de9a3423112e82
Mahdi Sanatkar <sanatkar.dev@gmail.com>
2022-09-06 09:57:35 +0430

    create ehraz-6

--- apps/tobank-app/pages/app/ehraz-6/index.tsx
+++ apps/tobank-app/pages/app/ehraz-6/index.tsx
@@ -3,9 +3,9 @@ import { items } from '../../../mocks/menu-data';
 import { Space } from 'antd';
 
 /* eslint-disable-next-line */
-export interface Ehraz4Props {}
+export interface Ehraz6Props {}
 
-export function Ehraz4(props: Ehraz4Props) {
+export function Ehraz6(props: Ehraz6Props) {
   return (
     <>
       <Space direction={'vertical'} size={32} className="pb-3">
@@ -25,19 +25,10 @@ export function Ehraz4(props: Ehraz4Props) {
                   style={{ width: '5px', background: '#707070', height: '2px' }}
                 ></div>
                 <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه کاملا واضح باشد
+                  تصویر کارت ملی کاملا واضح باشد
                 </small>
               </Space>
-              <Space size={5} align={'center'}>
-                <div
-                  style={{ width: '5px', background: '#707070', height: '2px' }}
-                ></div>
-                <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه کاملا واضح باشد
-                </small>
-              </Space>
-              <img src="/app/Group 12666.png" className="img-fluid" alt="" />
-              <img src="/app/شناسنامه (4).png" className="img-fluid" alt="" />
+              <img src="/app/Group 12658.png" className="img-fluid" alt="" />
             </Space>
           </Space>
         </Space>
@@ -57,18 +48,10 @@ export function Ehraz4(props: Ehraz4Props) {
                   style={{ width: '5px', background: '#707070', height: '2px' }}
                 ></div>
                 <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه کاملا واضح باشد
+                  تصویر کارت ملی تار و ناخوانا نباشد
                 </small>
               </Space>
-              <Space size={5} align={'center'}>
-                <div
-                  style={{ width: '5px', background: '#707070', height: '2px' }}
-                ></div>
-                <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه به صورت افقی قابل قبول نیست
-                </small>
-              </Space>
-              <img src="/app/شناسنامه (3).png" className="img-fluid" alt="" />
+              <img src="/app/Group 12657.png" className="img-fluid" alt="" />
             </Space>
           </Space>
           <Space align={'center'}>
@@ -78,18 +61,27 @@ export function Ehraz4(props: Ehraz4Props) {
                   style={{ width: '5px', background: '#707070', height: '2px' }}
                 ></div>
                 <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه کاملا واضح باشد
+                  تصویر کارت ملی به صورت کامل در کادر قرار بگیرد
                 </small>
               </Space>
+              <img src="/app/Group 12659.png" className="img-fluid" alt="" />
+            </Space>
+          </Space>
+          <Space align={'center'}>
+            <Space direction={'vertical'} align={'center'} size={10}>
               <Space size={5} align={'center'}>
                 <div
                   style={{ width: '5px', background: '#707070', height: '2px' }}
                 ></div>
                 <small style={{ fontSize: '10px' }}>
-                  تصویر شناسنامه به صورت کامل در کادر قرار بگیرد
+                  تصویر کارت ملی قدیمی قابل قبول نمی‌باشد
                 </small>
               </Space>
-              <img src="/app/Group 12671.png" className="img-fluid" alt="" />
+              <img
+                src="/app/تصویر روی کارت ملی قدیمی.png"
+                className="img-fluid"
+                alt=""
+              />
             </Space>
           </Space>
         </Space>
@@ -98,7 +90,7 @@ export function Ehraz4(props: Ehraz4Props) {
   );
 }
 
-export default Ehraz4;
+export default Ehraz6;
 
 export const getStaticProps = async () => {
   return {
