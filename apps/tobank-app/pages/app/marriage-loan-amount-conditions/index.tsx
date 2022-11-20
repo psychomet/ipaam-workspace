f04b2e5572109c11d5c17bdc3f65f3501c021870
Mahdi Sanatkar <sanatkar.dev@gmail.com>
2022-11-20 11:36:09 +0330

    complete marriage-loan-amount-conditions and marriage-loan-conditions

--- apps/tobank-app/pages/app/marriage-loan-amount-conditions/index.tsx
+++ apps/tobank-app/pages/app/marriage-loan-amount-conditions/index.tsx
@@ -1,116 +1,298 @@
-
-
-
 import styles from './index.module.less';
-import {Card, Col, Descriptions, Divider, Layout, PageHeader, Row, Typography} from 'antd';
+import {
+  Card,
+  Col,
+  Descriptions,
+  Divider,
+  Layout,
+  PageHeader,
+  Row,
+  Space,
+  Typography,
+} from 'antd';
 import classNames from 'classnames';
+
 const { Content } = Layout;
-const { Title,Text } = Typography;
+const { Title, Text } = Typography;
+
 /* eslint-disable-next-line */
 export interface MarriageLoanAmountConditionsProps {}
 
-
-export function MarriageLoanAmountConditions(props: MarriageLoanAmountConditionsProps) {
+export function MarriageLoanAmountConditions(
+  props: MarriageLoanAmountConditionsProps
+) {
   return (
     <div>
-      <PageHeader
-        title=' تسهیلات قرض‌الحسنه ازدواج'>
-      </PageHeader>
+      <Space direction={'vertical'} size={16}>
+        <h1 className={styles.title}>تسهیلات قرض‌الحسنه ازدواج</h1>
+        <div className={styles.card_feature}>
+          <Space direction={'vertical'} className={'w-100'}>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ تسهیلات{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>1،200،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ گواهی کسر از حقوق{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>1،464،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}> مدت </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>120 ماه</strong>
+              </span>
+            </section>
+            <div>
+              <span className={styles.card_feature__text__desc__label}>
+                <strong>توضیحات:</strong>
+              </span>
+              <span className={styles.card_feature__text__value}>
+                به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401
+                کل کشور، به هر یک از زوجین مبلغ 120 .ميليون تومان تسهیلات
+                قرض‌الحسنه ازدواج تعلق می گیرد
+              </span>
+            </div>
+          </Space>
+        </div>
+        <div className={styles.card_feature}>
+          <Space direction={'vertical'} className={'w-100'}>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ تسهیلات{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>1،500،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ گواهی کسر از حقوق{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>1،830،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}> مدت </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>120 ماه</strong>
+              </span>
+            </section>
+            <div>
+              <span className={styles.card_feature__text__desc__label}>
+                <strong>توضیحات:</strong>
+              </span>
+              <span className={styles.card_feature__text__value}>
+                به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401
+                کل کشور، به متقاضیانی که سن ازدواج آنها در زوج ها زیر 25 سال و
+                در زوجه ها زیر 23 سال می باشد، مبلغ 150 ميليون تومان تسهیلات
+                قرض‌الحسنه ازدواج تعلق می گیرد
+              </span>
+            </div>
+          </Space>
+        </div>
+        <div className={styles.card_feature}>
+          <Space direction={'vertical'} className={'w-100'}>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ تسهیلات{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>2،400،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ گواهی کسر از حقوق{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>2،928،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}> مدت </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>120 ماه</strong>
+              </span>
+            </section>
+            <div>
+              <span className={styles.card_feature__text__desc__label}>
+                <strong>توضیحات:</strong>
+              </span>
+              <span className={styles.card_feature__text__value}>
+                به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401
+                کل کشور، به هر یک از زوجین که مشمول ماده (50) قانون جامع خدمات
+                رساني به ايثارگران باشند، مبلغ 240 ميليون تومان تسهیلات
+                قرض‌الحسنه ازدواج تعلق می‌گیرد
+              </span>
+            </div>
+          </Space>
+        </div>
+        <div className={styles.card_feature}>
+          <Space direction={'vertical'} className={'w-100'}>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ تسهیلات{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>3،000،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}>
+                {' '}
+                مبلغ گواهی کسر از حقوق{' '}
+              </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>3،660،000،000 ریال</strong>
+              </span>
+            </section>
+            <section className={styles.card_feature__text}>
+              <span className={styles.card_feature__text__label}> مدت </span>
+              <span className={styles.card_feature__text__divider}></span>
+              <span className={styles.card_feature__text__value}>
+                <strong>120 ماه</strong>
+              </span>
+            </section>
+            <div>
+              <span className={styles.card_feature__text__desc__label}>
+                <strong>توضیحات:</strong>
+              </span>
+              <span className={styles.card_feature__text__value}>
+                به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401
+                کل کشور، به متقاضیانی که سن ازدواج آنها در زوج ها زیر 25 سال و
+                در زوجه ها زیر 23 سال می باشد و مشمول ماده (50) قانون جامع خدمات
+                رساني به ايثارگران باشند، مبلغ 300 ميليون تومان تسهیلات
+                قرض‌الحسنه ازدواج .تعلق می گیرد{' '}
+              </span>
+            </div>
+          </Space>
+        </div>
+      </Space>
 
-      <Content className='my-4'>
-          <Row gutter={[20,20]}>
-            <Col md={12} >
-              <Descriptions title="User Info" column={1}>
+      {/*<PageHeader*/}
+      {/*  title=' تسهیلات قرض‌الحسنه ازدواج'>*/}
+      {/*</PageHeader>*/}
 
-                  <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+      {/*<Content className='my-4'>*/}
+      {/*    <Row gutter={[20,20]}>*/}
+      {/*      <Col md={12} >*/}
+      {/*        <Descriptions title="User Info" column={1}>*/}
 
-                  <Descriptions.Item label="Telephone">1810000000 توضیحات:</Descriptions.Item>
+      {/*            <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>*/}
 
-                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-                <Descriptions.Item label="Remark">empty</Descriptions.Item>
-                <Descriptions.Item label="Address">
-                  No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                </Descriptions.Item>
-              </Descriptions>
-            </Col>
-            <Col md={12}>
-              <Card title="مبالغ تسهیلات ازدواج">
-                <Row>
-                  <Col>
-                    <p>
-                      <Text strong>مبلغ تسهیلات: </Text>
-                      1،500،000،000ریال
-                    </p>
-                    <p>
-                      <Text strong>مبلغ گواهی کسر از حقوق: </Text>
-                      1،830،000،000ریال</p>
-                    <p>
-                      <Text strong> مدت: </Text>
-                      120 ماه
-                    </p>
-                  </Col>
-                  <Col>
-                    <Divider/>
-                    <Text strong>توضیحات: </Text>
-                    توضیحات: به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401 کل کشور، به متقاضیانی که سن ازدواج آنها در زوج ها زیر 25 سال و در زوجه ها زیر 23 سال می باشد، مبلغ 150 ميليون تومان تسهیلات قرض‌الحسنه ازدواج تعلق می گیرد.
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-            <Col md={12}>
-              <Card title="مبالغ تسهیلات ازدواج">
-                <Row>
-                  <Col>
-                    <p>
-                      <Text strong>مبلغ تسهیلات: </Text>
-                      2،400،000،000ریال
-                    </p>
-                    <p>
-                      <Text strong>مبلغ گواهی کسر از حقوق: </Text>
-                      2،928،000،000ریال</p>
-                    <p>
-                      <Text strong> مدت: </Text>
-                      120 ماه
-                    </p>
-                  </Col>
-                  <Col>
-                    <Divider/>
-                    <Text strong>توضیحات: </Text>
-                    توضیحات:  به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401 کل کشور،  به هر یک از زوجین که مشمول ماده (50) قانون جامع خدمات رساني به ايثارگران باشند، مبلغ  240 ميليون تومان تسهیلات قرض‌الحسنه ازدواج تعلق می‌گیرد.                  </Col>
-                </Row>
-              </Card>
-            </Col>
-            <Col md={12}>
-              <Card title="مبالغ تسهیلات ازدواج">
-                <Row>
-                  <Col>
-                    <p>
-                      <Text strong>مبلغ تسهیلات: </Text>
-                      3،000،000،000ریال
-                    </p>
-                    <p>
-                      <Text strong>مبلغ گواهی کسر از حقوق: </Text>
-                      3،660،000،000ریال</p>
-                    <p>
-                      <Text strong> مدت: </Text>
-                      120 ماه
-                    </p>
-                  </Col>
-                  <Col>
-                    <Divider/>
-                    <Text strong>توضیحات: </Text>
-                    توضیحات:  به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون
-                    بودجه سال 1401 کل کشور، به متقاضیانی که سن ازدواج آنها
-                    در زوج ها زیر 25 سال و در زوجه ها زیر 23 سال می باشد و
-                    مشمول ماده (50) قانون جامع خدمات رساني به ايثارگران
-                    باشند، مبلغ  300 ميليون تومان تسهیلات قرض‌الحسنه ازدواج
-                    تعلق می گیرد.                  </Col>
-                </Row>
-              </Card>
-            </Col>
-          </Row>
+      {/*            <Descriptions.Item label="Telephone">1810000000 توضیحات:</Descriptions.Item>*/}
 
-        </Content>
+      {/*          <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>*/}
+      {/*          <Descriptions.Item label="Remark">empty</Descriptions.Item>*/}
+      {/*          <Descriptions.Item label="Address">*/}
+      {/*            No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China*/}
+      {/*          </Descriptions.Item>*/}
+      {/*        </Descriptions>*/}
+      {/*      </Col>*/}
+      {/*      <Col md={12}>*/}
+      {/*        <Card title="مبالغ تسهیلات ازدواج">*/}
+      {/*          <Row>*/}
+      {/*            <Col>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ تسهیلات: </Text>*/}
+      {/*                1،500،000،000ریال*/}
+      {/*              </p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ گواهی کسر از حقوق: </Text>*/}
+      {/*                1،830،000،000ریال</p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong> مدت: </Text>*/}
+      {/*                120 ماه*/}
+      {/*              </p>*/}
+      {/*            </Col>*/}
+      {/*            <Col>*/}
+      {/*              <Divider/>*/}
+      {/*              <Text strong>توضیحات: </Text>*/}
+      {/*              توضیحات: به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401 کل کشور، به متقاضیانی که سن ازدواج آنها در زوج ها زیر 25 سال و در زوجه ها زیر 23 سال می باشد، مبلغ 150 ميليون تومان تسهیلات قرض‌الحسنه ازدواج تعلق می گیرد.*/}
+      {/*            </Col>*/}
+      {/*          </Row>*/}
+      {/*        </Card>*/}
+      {/*      </Col>*/}
+      {/*      <Col md={12}>*/}
+      {/*        <Card title="مبالغ تسهیلات ازدواج">*/}
+      {/*          <Row>*/}
+      {/*            <Col>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ تسهیلات: </Text>*/}
+      {/*                2،400،000،000ریال*/}
+      {/*              </p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ گواهی کسر از حقوق: </Text>*/}
+      {/*                2،928،000،000ریال</p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong> مدت: </Text>*/}
+      {/*                120 ماه*/}
+      {/*              </p>*/}
+      {/*            </Col>*/}
+      {/*            <Col>*/}
+      {/*              <Divider/>*/}
+      {/*              <Text strong>توضیحات: </Text>*/}
+      {/*              توضیحات:  به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون بودجه سال 1401 کل کشور،  به هر یک از زوجین که مشمول ماده (50) قانون جامع خدمات رساني به ايثارگران باشند، مبلغ  240 ميليون تومان تسهیلات قرض‌الحسنه ازدواج تعلق می‌گیرد.                  </Col>*/}
+      {/*          </Row>*/}
+      {/*        </Card>*/}
+      {/*      </Col>*/}
+      {/*      <Col md={12}>*/}
+      {/*        <Card title="مبالغ تسهیلات ازدواج">*/}
+      {/*          <Row>*/}
+      {/*            <Col>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ تسهیلات: </Text>*/}
+      {/*                3،000،000،000ریال*/}
+      {/*              </p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong>مبلغ گواهی کسر از حقوق: </Text>*/}
+      {/*                3،660،000،000ریال</p>*/}
+      {/*              <p>*/}
+      {/*                <Text strong> مدت: </Text>*/}
+      {/*                120 ماه*/}
+      {/*              </p>*/}
+      {/*            </Col>*/}
+      {/*            <Col>*/}
+      {/*              <Divider/>*/}
+      {/*              <Text strong>توضیحات: </Text>*/}
+      {/*              توضیحات:  به استناد مفاد جزء (1) بند (الف) تبصره 16 قانون*/}
+      {/*              بودجه سال 1401 کل کشور، به متقاضیانی که سن ازدواج آنها*/}
+      {/*              در زوج ها زیر 25 سال و در زوجه ها زیر 23 سال می باشد و*/}
+      {/*              مشمول ماده (50) قانون جامع خدمات رساني به ايثارگران*/}
+      {/*              باشند، مبلغ  300 ميليون تومان تسهیلات قرض‌الحسنه ازدواج*/}
+      {/*              تعلق می گیرد.                  </Col>*/}
+      {/*          </Row>*/}
+      {/*        </Card>*/}
+      {/*      </Col>*/}
+      {/*    </Row>*/}
+
+      {/*  </Content>*/}
     </div>
   );
 }
--- apps/tobank-app/pages/app/marriage-loan-conditions/index.tsx
+++ apps/tobank-app/pages/app/marriage-loan-conditions/index.tsx
@@ -1,60 +1,72 @@
 import styles from './index.module.less';
-import {Card, Col, Divider, Layout, List, PageHeader, Row, Table, Typography} from "antd";
+import {
+  Card,
+  Col,
+  Divider,
+  Layout,
+  List,
+  PageHeader,
+  Row,
+  Space,
+  Table,
+  Typography,
+} from 'antd';
+
 const { Content } = Layout;
-const { Title,Text } = Typography;
+const { Title, Text } = Typography;
+
 /* eslint-disable-next-line */
 export interface MarriageLoanConditionsProps {}
 
 const data = [
-  '* تصویر سند ازدواج (صفحات 2-3-4-5-19 عقدنامه)',
-  '* تصویر صفحات شناسنامه متقاضی، همسر و ضامن',
-  '* تصویر روی و پشت کارت ملی متقاضی، همسر و ضامن ',
-  '* نشانی کامل پستی، کدپستی دقیق محل سکونت متقاضی و ضامن',
-  '* کد رهگیری ده رقمی',
-  '* تصویر چک وثیقه',
-  '* تصویر گواهی کسر از حقوق رسمی بابت تضمین',
+  'تصویر سند ازدواج (صفحات 2-3-4-5-19 عقدنامه)',
+  'تصویر صفحات شناسنامه متقاضی، همسر و ضامن',
+  'تصویر روی و پشت کارت ملی متقاضی، همسر و ضامن ',
+  'نشانی کامل پستی، کدپستی دقیق محل سکونت متقاضی و ضامن',
+  'کد رهگیری ده رقمی',
+  'تصویر چک وثیقه',
+  'تصویر گواهی کسر از حقوق رسمی بابت تضمین',
 ];
 
 const data2 = [
-  '* برای دریافت تسهیلات بایستی در سامانه تسهیلات قرض\n' +
-  ' الحسنه ازدواج، شعبه مرکزی بانک گردشگری را به عنوان شعبه عامل\n' +
-  'اعطاي وام خود انتخاب کرده و کد رهگیری دریافت کرده باشید.',
-  '* حداقل مبلغ مورد نیاز برای افتتاح سپرده قرض‌الحسنه پس‌انداز 1.500.000 ریال می باشد که کارمزدهای مورد نیاز برای اعطای تسهیلات ازاین مبلغ کسر می‌گردد.',
-  '* وثایق مطابق با ضوابط و قوانین ابلاغی و بر اساس اعتبارسنجی متقاضی\n' +
-  'و ضامن تعیین و اخذ می‌گردند. ',
-  '* اصل مدارک و وثایق می‌بایست در روز پرداخت تسهیلات به بانک ارائه\n' +
-  ' گردند. زمان و مکان مراجعه حضوری پس از انجام ثبت نام و بررسی\n' +
-  ' مدارک و مستندات توسط بانک و تصویب پرداخت تسهیلات به اطلاع\n' +
-  'شما خواهد رسید.',
-  '* لطفاً برای پیگیری روند پرونده پس از ثبت نام به صورت روزانه به\n' +
-  'کارتابل خود در سامانه توبانک مراجعه فرمایید.',
-
+  'برای دریافت تسهیلات بایستی در سامانه تسهیلات قرض\n' +
+    ' الحسنه ازدواج، شعبه مرکزی بانک گردشگری را به عنوان شعبه عامل\n' +
+    'اعطاي وام خود انتخاب کرده و کد رهگیری دریافت کرده باشید.',
+  ' حداقل مبلغ مورد نیاز برای افتتاح سپرده قرض‌الحسنه پس‌انداز 1.500.000 ریال می باشد که کارمزدهای مورد نیاز برای اعطای تسهیلات ازاین مبلغ کسر می‌گردد.',
+  ' وثایق مطابق با ضوابط و قوانین ابلاغی و بر اساس اعتبارسنجی متقاضی\n' +
+    'و ضامن تعیین و اخذ می‌گردند. ',
+  ' اصل مدارک و وثایق می‌بایست در روز پرداخت تسهیلات به بانک ارائه\n' +
+    ' گردند. زمان و مکان مراجعه حضوری پس از انجام ثبت نام و بررسی\n' +
+    ' مدارک و مستندات توسط بانک و تصویب پرداخت تسهیلات به اطلاع\n' +
+    'شما خواهد رسید.',
+  ' لطفاً برای پیگیری روند پرونده پس از ثبت نام به صورت روزانه به\n' +
+    'کارتابل خود در سامانه توبانک مراجعه فرمایید.',
 ];
 
 const dataSource = [
   {
     key: '1',
-    amount: '1،200،000،000',
-    salary: '1،464،000،000',
-    time: '‏120 ماه',
+    amount: <strong>1،200،000،000</strong>,
+    salary: <strong>1،464،000،000</strong>,
+    time: <strong>120 ماه</strong>,
   },
   {
     key: '2',
-    amount: '1،500،000،000',
-    salary: '1،830،000،000',
-    time: '‏120 ماه',
+    amount: <strong>1،500،000،000</strong>,
+    salary: <strong>1،830،000،000</strong>,
+    time: <strong>120 ماه</strong>,
   },
   {
     key: '3',
-    amount: '2،400،000،000',
-    salary: '2،928،000،000',
-    time: '‏120 ماه',
+    amount: <strong>2،400،000،000</strong>,
+    salary: <strong>2،928،000،000</strong>,
+    time: <strong>120 ماه</strong>,
   },
   {
     key: '4',
-    amount: '3،000،000،000',
-    salary: '3،660،000،000',
-    time: '‏120 ماه',
+    amount: <strong>3،000،000،000</strong>,
+    salary: <strong>3،660،000،000</strong>,
+    time: <strong>120 ماه</strong>,
   },
 ];
 
@@ -76,52 +88,61 @@ const columns = [
   },
 ];
 
-
 export function MarriageLoanConditions(props: MarriageLoanConditionsProps) {
   return (
-    <div className='container'>
-      <PageHeader
-        title='شرایط و مدارک موردنیاز درخواست تسهیلات ازدواج'>
-      </PageHeader>
-
-      <Content className='my-4'>
-        <Row gutter={[20,20]}>
-          <Col span={24}>
-            <List
-              header={<Text strong style={{lineHeight:2}}>مدارک موردنیاز برای ثبت‌ درخواست <p>لطفا جهت ثبت درخواست مدارک زیر را آماده داشته ‌باشید</p></Text>}
-              size="large"
-              dataSource={data}
-              renderItem={(item) => <List.Item>{item}</List.Item>}
-              split={false}
-              bordered={false}
-            ></List>
-          </Col>
-          <Col span={24}>
-            <Title level={5}>
-              مبالغ تسهیلات ازدواج
-            </Title>
-            <p>
-              *مبالغ به ریال می‌باشند
-            </p>
-            <Table dataSource={dataSource} columns={columns}  pagination={false}
-            />
-          </Col>
-          <Col span={24}>
-            <List
-              header={<Text strong style={{lineHeight:2}}>توضیحات تکمیل</Text>}
-              size="large"
-              dataSource={data2}
-              renderItem={(item) => <List.Item>{item}</List.Item>}
-              split={false}
-              bordered={false}
-            ></List>
-          </Col>
-
-        </Row>
-
-
-
-      </Content>
+    <div>
+      <Space direction="vertical">
+        <span className={styles.title}>
+          شرایط و مدارک موردنیاز درخواست تسهیلات ازدواج
+        </span>
+        <span className={styles.subtitle}>
+          مدارک موردنیاز برای ثبت‌ درخواست
+        </span>
+        <ul className={styles.list}>
+          {data.map((d) => (
+            <li>{d}</li>
+          ))}
+        </ul>
+        <span className={styles.title2}>مبالغ تسهیلات ازدواج</span>
+        <span style={{ fontSize: 10 }}>مبالغ به ریال می‌باشند*</span>
+        {/*<List*/}
+        {/*  header={*/}
+        {/*    <Text strong style={{ lineHeight: 2 }}>*/}
+        {/*      مدارک موردنیاز برای ثبت‌ درخواست{' '}*/}
+        {/*      <p>لطفا جهت ثبت درخواست مدارک زیر را آماده داشته ‌باشید</p>*/}
+        {/*    </Text>*/}
+        {/*  }*/}
+        {/*  size="large"*/}
+        {/*  dataSource={data}*/}
+        {/*  renderItem={(item) => <List.Item>{item}</List.Item>}*/}
+        {/*  split={false}*/}
+        {/*  bordered={false}*/}
+        {/*></List>*/}
+        <Text strong style={{ lineHeight: 2 }}>
+          توضیحات تکمیل
+        </Text>
+        <Table
+          style={{ fontSize: 12 }}
+          dataSource={dataSource}
+          columns={columns}
+          pagination={false}
+        />
+        <ul className={styles.list}>
+          {data2.map((d) => (
+            <li>{d}</li>
+          ))}
+        </ul>
+        {/*<List*/}
+        {/*  header={*/}
+        {/*    */}
+        {/*  }*/}
+        {/*  size="large"*/}
+        {/*  dataSource={data2}*/}
+        {/*  renderItem={(item) => <List.Item>{item}</List.Item>}*/}
+        {/*  split={false}*/}
+        {/*  bordered={false}*/}
+        {/*></List>*/}
+      </Space>
     </div>
   );
 }
