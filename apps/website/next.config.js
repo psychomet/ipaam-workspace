161ac1c79943aad75083fb6c647e7919dafcc582
Mahdi Sanatkar <sanatkar.dev@gmail.com>
2022-11-28 11:05:31 +0330

    Initial commit

---
+++ apps/website/next.config.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withNx } = require('@nrwl/next/plugins/with-nx');
+
+// This plugin is needed until this PR is merged.
+// https://github.com/vercel/next.js/pull/23185
+const { withLess } = require('@nrwl/next/plugins/with-less');
+
+/**
+ * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+module.exports = withLess(withNx(nextConfig));
